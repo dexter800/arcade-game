be2a887ed97f79f47ed2f9651de136193c2f48d1
Pratirodh <dexterprasai@gmail.com>
2018-10-09 23:55:07 +1100

    file change

--- js/app.js
+++ js/app.js
@@ -135,12 +135,20 @@ enemyPosition.forEach(function (posY) {
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function (e) {
+
+
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
-    };
 
+    };
     player.handleInput(allowedKeys[e.keyCode]);
+
+    // let newNum = Object.keys(allowedKeys);
+    // console.log(newNum);
+
+    // const audio = document.querySelector(`audio[${} = "${e.keyCode}"]`);
+
 });
